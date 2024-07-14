3aa967e492bbf75391caf62bf92285a0fa87fc43
Bullets-jyr <jyr@bullets.co.kr>
2024-07-14 14:06:00 +0900

    View 구현 - 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wabiz_mvvm/view_model/post_view_model.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,10 +15,89 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Consumer(
+          builder: (context, ref, child) {
+            // final viewModel = ref.read(postViewModelProvider);
+            // final posts = ref.watch(fetchPostsProvider);
+            // final posts = ref.watch(fetchPostsProvider2);
+            // final posts = ref.watch(asyncPostAsyncNotifier);
+            final posts = ref.watch(asyncPostsGenNotifierProvider);
+            return posts.when(
+              data: (data) {
+                return ListView.builder(
+                  itemCount: data?.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        '${data?[index].id}',
+                      ),
+                      subtitle: Text(
+                        '${data?[index].title}',
+                      ),
+                    );
+                  },
+                );
+              },
+              error: (error, trace) {
+                return Text('$error');
+              },
+              loading: () {
+                return Center(
+                  child: CircularProgressIndicator.adaptive(),
+                );
+              },
+            );
+            // return switch (posts) {
+            //   AsyncData(:final value) => ListView.builder(
+            //       itemCount: value?.length,
+            //       itemBuilder: (context, index) {
+            //         return ListTile(
+            //           title: Text(
+            //             '${value?[index].id}',
+            //           ),
+            //           subtitle: Text(
+            //             '${value?[index].title}',
+            //           ),
+            //         );
+            //       },
+            //     ),
+            //   AsyncError(:final error) => Text('$error'),
+            //   _ => Center(
+            //       child: CircularProgressIndicator.adaptive(),
+            //     ),
+            // };
+            // return FutureBuilder(
+            //   future: viewModel.getPosts(),
+            //   builder: (context, snapshot) {
+            //     if (snapshot.hasData) {
+            //       final value = snapshot.data ?? [];
+            //       if (value.isEmpty) {
+            //         return Center(
+            //           child: Text('아이템이 없습니다.'),
+            //         );
+            //       }
+            //       return ListView.builder(
+            //         itemCount: value.length,
+            //         itemBuilder: (context, index) {
+            //           return ListTile(
+            //             title: Text(
+            //               '${value[index].id}',
+            //             ),
+            //             subtitle: Text(
+            //               '${value[index].title}',
+            //             ),
+            //           );
+            //         },
+            //       );
+            //     }
+            //     return Center(
+            //       child: CircularProgressIndicator.adaptive(),
+            //     );
+            //   },
+            // );
+          },
         ),
       ),
     );
